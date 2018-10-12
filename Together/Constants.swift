a9fc781c0c517343f32a8c7f8cd7ccf6c8d689ff
Alek Matthiessen <alekmatthiessen@aleks-macbook-pro.local>
2018-10-12 16:22:39 -0400

    test

---
+++ Together/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Together
+//
+//  Created by Alek Matthiessen on 10/12/18.
+//  Copyright © 2018 AA Tech. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+struct Constants
+{
+    struct refs
+    {
+        static let databaseRoot = Database.database().reference()
+        static let databaseChats = databaseRoot.child("Lonely")
+    }
+}
