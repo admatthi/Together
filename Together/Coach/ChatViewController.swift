98cdf9c2948559b8057396f9106e70ee925945f6
Alek Matthiessen <alekmatthiessen@aleks-macbook-pro.local>
2019-04-11 08:28:48 -0700

    loljk

--- Together/Coach/ChatViewController.swift
+++ Together/Coach/ChatViewController.swift
@@ -9,9 +9,171 @@
 import UIKit
 import IQKeyboardManager
 import Firebase
+import MessageKit
+import MessageInputBar
 
 var myorange = UIColor(red:0.99, green:0.51, blue:0.30, alpha:1.0)
 
-class ChatViewController: UIViewController {
+class ChatViewController: MessagesViewController, MessagesDataSource  {
+    
+    var messageList: [MockMessage] = []
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureMessageCollectionView()
+        configureMessageInputBar()
+        loadFirstMessages()
+        messagesCollectionView.messagesLayoutDelegate = self
+
+    }
+ 
+    func currentSender() -> Sender {
+        return SampleData.shared.currentSender
+    }
+    
+    func loadFirstMessages() {
+        DispatchQueue.global(qos: .userInitiated).async {
+            let count = UserDefaults.standard.mockMessagesCount()
+            SampleData.shared.getMessages(count: count) { messages in
+                DispatchQueue.main.async {
+                    self.messageList = messages
+                    self.messagesCollectionView.reloadData()
+//                    self.messagesCollectionView.scrollToBottom()
+                }
+            }
+        }
+    }
+    
+    let refreshControl = UIRefreshControl()
+
+    func configureMessageCollectionView() {
+        
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messageCellDelegate = self
+        
+        scrollsToBottomOnKeyboardBeginsEditing = true // default false
+        maintainPositionOnKeyboardFrameChanged = true // default false
+        
+        messagesCollectionView.addSubview(refreshControl)
+        refreshControl.addTarget(self, action: #selector(loadMoreMessages), for: .valueChanged)
+    }
+    
+    @objc func loadMoreMessages() {
+        DispatchQueue.global(qos: .userInitiated).asyncAfter(deadline: .now() + 1) {
+            SampleData.shared.getMessages(count: 20) { messages in
+                DispatchQueue.main.async {
+                    self.messageList.insert(contentsOf: messages, at: 0)
+                    self.messagesCollectionView.reloadDataAndKeepOffset()
+                    self.refreshControl.endRefreshing()
+                }
+            }
+        }
+    }
+    
+    func configureMessageInputBar() {
+        messageInputBar.delegate = self
+        messageInputBar.inputTextView.tintColor = .white
+        messageInputBar.sendButton.tintColor = .blue
+    }
+    
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messageList.count
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messageList[indexPath.section]
+    }
+    
+    func insertMessage(_ message: MockMessage) {
+        messageList.append(message)
+        // Reload last section to update header/footer labels and insert a new one
+        messagesCollectionView.performBatchUpdates({
+            messagesCollectionView.insertSections([messageList.count - 1])
+            if messageList.count >= 2 {
+                messagesCollectionView.reloadSections([messageList.count - 2])
+            }
+        }, completion: { [weak self] _ in
+            if self?.isLastSectionVisible() == true {
+                self?.messagesCollectionView.scrollToBottom(animated: true)
+            }
+        })
+    }
+    
+    func isLastSectionVisible() -> Bool {
+        
+        guard !messageList.isEmpty else { return false }
+        
+        let lastIndexPath = IndexPath(item: 0, section: messageList.count - 1)
+        
+        return messagesCollectionView.indexPathsForVisibleItems.contains(lastIndexPath)
+    }
+    
+}
+
+
+extension ChatViewController: MessageInputBarDelegate {
+    
+    func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
+        
+        for component in inputBar.inputTextView.components {
+            
+            if let str = component as? String {
+                let message = MockMessage(text: str, sender: currentSender(), messageId: UUID().uuidString, date: Date())
+                insertMessage(message)
+            } else if let img = component as? UIImage {
+                let message = MockMessage(image: img, sender: currentSender(), messageId: UUID().uuidString, date: Date())
+                insertMessage(message)
+            }
+            
+        }
+        inputBar.inputTextView.text = String()
+        messagesCollectionView.scrollToBottom(animated: true)
+    }
+    
+}
+
+extension ChatViewController: MessageCellDelegate {
+    
+    func didTapAvatar(in cell: MessageCollectionViewCell) {
+        print("Avatar tapped")
+    }
+    
+    func didTapMessage(in cell: MessageCollectionViewCell) {
+        print("Message tapped")
+    }
+    
+    func didTapCellTopLabel(in cell: MessageCollectionViewCell) {
+        print("Top cell label tapped")
+    }
+    
+    func didTapMessageTopLabel(in cell: MessageCollectionViewCell) {
+        print("Top message label tapped")
+    }
+    
+    func didTapMessageBottomLabel(in cell: MessageCollectionViewCell) {
+        print("Bottom label tapped")
+    }
+    
+    func didTapAccessoryView(in cell: MessageCollectionViewCell) {
+        print("Accessory view tapped")
+    }
+    
+}
+
+extension ChatViewController: MessagesLayoutDelegate {
+    
+    func cellTopLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+        return 18
+    }
+    
+    func messageTopLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+        return 20
+    }
+    
+    func messageBottomLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+        return 16
+    }
+    
 }
